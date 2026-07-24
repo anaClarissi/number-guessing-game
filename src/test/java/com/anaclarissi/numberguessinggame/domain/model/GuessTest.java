37ebd16f0c71fd4a8a91c210b07234207d34b360
Ana Clarissi <anaclarissicruz@gmail.com>
2026-07-23 23:55:12 -0300

    feat(domain.model): added unit tests to Guess class

---
+++ src/test/java/com/anaclarissi/numberguessinggame/domain/model/GuessTest.java
@@ -0,0 +1,68 @@
+package com.anaclarissi.numberguessinggame.domain.model;
+
+import com.anaclarissi.numberguessinggame.domain.exception.InvalidGuessException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class GuessTest {
+
+    @Test
+    void shouldReturnValidValues() {
+
+        int value = 50;
+        int attemptValue = 10;
+
+        Guess guess = new Guess(value, attemptValue);
+
+        assertEquals(value, guess.getValue());
+        assertEquals(attemptValue, guess.getAttemptNumber());
+
+    }
+
+    @Test
+    void shouldThrowAnInvalidGuessExceptionWhenTheValueIsLessThanOne() {
+
+        assertThrows(InvalidGuessException.class, () -> new Guess(0, 10));
+
+    }
+
+    @Test
+    void shouldThrowAnInvalidGuessExceptionWhenTheValueIsMoreThanOneHundred() {
+
+        assertThrows(InvalidGuessException.class, () -> new Guess(101, 10));
+
+    }
+
+    @Test
+    void shouldThrowAnInvalidGuessExceptionWhenTheAttemptValueIsLessThanOne() {
+
+        assertThrows(InvalidGuessException.class, () -> new Guess(50, 0));
+
+    }
+
+    @Test
+    void shouldNotThrowAnExceptionWhenTheValueIsOne() {
+
+        assertDoesNotThrow(() -> new Guess(1, 10));
+
+    }
+
+    @Test
+    void shouldNotThrowAnExceptionWhenTheValueIsOneHundred() {
+
+        assertDoesNotThrow(() -> new Guess(100, 10));
+
+    }
+
+    @Test
+    void shouldNotThrowAnExceptionWhenTheAttemptValueIsOne() {
+
+        assertDoesNotThrow(() -> new Guess(50, 1));
+
+    }
+
+
+}
