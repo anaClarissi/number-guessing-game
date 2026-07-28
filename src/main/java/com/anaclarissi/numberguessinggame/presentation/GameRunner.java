82baee8ee8f61a35fdb8e343cfb3c8470467a187
Ana Clarissi <anaclarissicruz@gmail.com>
2026-07-28 19:37:31 -0300

    feat(presentation): added GameRunner class

---
+++ src/main/java/com/anaclarissi/numberguessinggame/presentation/GameRunner.java
@@ -0,0 +1,56 @@
+package com.anaclarissi.numberguessinggame.presentation;
+
+import com.anaclarissi.numberguessinggame.application.GameService;
+import com.anaclarissi.numberguessinggame.domain.model.Difficulty;
+import com.anaclarissi.numberguessinggame.domain.model.GameRound;
+import com.anaclarissi.numberguessinggame.domain.model.GuessResult;
+
+public class GameRunner {
+
+    private final GameService gameService;
+
+    private final ConsoleView view;
+
+    private final ConsoleInputReader input;
+
+    public GameRunner(GameService gameService, ConsoleView view, ConsoleInputReader input) {
+        this.gameService = gameService;
+        this.view = view;
+        this.input = input;
+    }
+
+    public void run() {
+
+        view.showWelcome();
+
+        view.showDifficultyMenu();
+
+        Difficulty difficulty = input.readDifficultyChoice();
+
+        GameRound round = gameService.startNewRound(difficulty);
+
+        while (!round.isOver()) {
+
+            int guessValue = input.readGuess();
+
+            GuessResult result = gameService.processGuess(round, guessValue);
+
+            if (result == GuessResult.CORRECT) {
+
+                view.showVictory(round.getAttemptsUsed());
+
+            } else if (round.isOver()) {
+
+                view.showDefeat(round.revealSecretNumber());
+
+            } else {
+
+                view.showResult(result, guessValue);
+
+            }
+
+        }
+
+    }
+
+}
